843fe2827aeb2cc2c83dda638b54473ab13357c2
夕云葛城 <kuroneko7520@outlook.com>
2025-08-28 18:34:32 +0800

    refactor: :recycle: 将首屏加载动画时间添加到环境变量

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,4 @@
 <script setup lang="ts">
-import { useRequest } from 'alova/client'
-
-const { onSuccess } = useRequest(Apis.role.listRole({
-  transform: data => data.data ?? [],
-}))
-
-onSuccess((...args) => {
-  console.warn('list-role', { args })
-})
 </script>
 
 <template>
