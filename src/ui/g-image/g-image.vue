04a2486d83370955b39a812c8e95deb075984dd1
夕云葛城 <kuroneko7520@outlook.com>
2026-04-07 20:16:47 +0800

    feat(ui): :sparkles: [g-image] 添加图片组件

---
+++ src/ui/g-image/g-image.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+  src?: string
+}>()
+
+const loading = ref(Boolean(props.src?.trim()))
+const error = ref(false)
+</script>
+
+<template>
+  <div class="overflow-hidden flex" style="container-type: size">
+    <img
+      v-show="src?.trim() && !loading && !error"
+      :draggable="false"
+      :src="props.src"
+      class="flex-1 object-cover"
+      @load="loading = false"
+      @error="error = true"
+    />
+  </div>
+</template>
---
+++ src/ui/g-image/index.ts
@@ -0,0 +1,1 @@
+export { default as GImage } from './g-image.vue'
