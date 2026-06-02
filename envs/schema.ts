3395619f8907d7f14cef0cec74f293a4e1106174
夕云葛城 <kuroneko7520@outlook.com>
2026-06-02 16:24:55 +0800

    docs: 添加接口注释

--- envs/schema.ts
+++ envs/schema.ts
@@ -15,7 +15,7 @@ export const envSchema = z.object({
     .trim()
     .regex(/^[\w-]+:[\w-]+$/, '需符合格式 username:password')
     .meta({ description: '主服务 Basic 认证' }),
-  VITE_SERVICE_MAIN_PROXY: z.string().optional().meta({ description: '主服务代理URL' }),
+  VITE_SERVICE_MAIN_PROXY: z.string().optional().meta({ description: '主服务代理 URL' }),
   VITE_SERVICE_MAIN_OPENAPI_URL: z.string().optional().meta({ description: '主服务 OpenAPI URL' }),
 
   // 配置服务
@@ -23,15 +23,15 @@ export const envSchema = z.object({
     .string()
     .trim()
     .min(1, '配置服务 URL 不能为空')
-    .meta({ description: '配置服务URL' }),
-  VITE_SERVICE_CONFIG_PROXY: z.string().optional().meta({ description: '配置服务代理URL' }),
+    .meta({ description: '配置服务 URL' }),
+  VITE_SERVICE_CONFIG_PROXY: z.string().optional().meta({ description: '配置服务代理 URL' }),
 
   // 资源服务
   VITE_SERVICE_RESOURCE_URL: z
     .string()
     .trim()
     .min(1, '资源服务 URL 不能为空')
-    .meta({ description: '资源服务URL' }),
+    .meta({ description: '资源服务 URL' }),
   VITE_SERVICE_RESOURCE_PROXY: z.string().optional().meta({ description: '资源服务代理URL' }),
 })
 
