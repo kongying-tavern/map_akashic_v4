da7bf2e14fe018ab0a585cb8a51ddc3640efe7a0
夕云葛城 <kuroneko7520@outlook.com>
2026-06-03 20:07:06 +0800

    feat(sider-menus/item-filter): ✨ add area select placeholder and integrate alova request
    首先新增了 alova 请求库的导入和区域列表接口调用，添加了占位用的地区选择展示容器，同时优化了原有样式的计算方式

--- src/feature/sider-menus/item-filter/index.vue
+++ src/feature/sider-menus/item-filter/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useRequest } from 'alova/client'
+import Api from '@/api'
 import WinuiSegmented from '@/ui/winui/winui-segmented.vue'
 
 const mode = ref('normal')
@@ -6,11 +8,28 @@ const options = [
   { label: '效率', value: 'normal' },
   { label: '进阶', value: 'advencend' },
 ]
+
+const { data } = useRequest(
+  Api.main.area.listArea({
+    data: {
+      isTraverse: true,
+      parentId: -1,
+    },
+  }),
+  {
+    initialData: {},
+  },
+)
 </script>
 
 <template>
   <div class="flex-1 w-full" style="container-type: inline-size">
     <!-- 模式切换 -->
-    <WinuiSegmented v-model="mode" :options="options" class="w-94cqw m-3cqw" />
+    <WinuiSegmented v-model="mode" :options="options" class="w-[calc(100%-1.5rem)] m-3" />
+
+    <!-- 地区选择 TODO -->
+    <div class="border border-[--gl-4] bg-[--gl-2] w-[calc(100%-1.5rem)] mx-3 h-20 rounded-lg">
+      {{ data.data?.length }}
+    </div>
   </div>
 </template>
