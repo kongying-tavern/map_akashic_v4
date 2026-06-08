4448c11426301b3d2c1025396a66ddc5abe07e08
夕云葛城 <kuroneko7520@outlook.com>
2026-06-08 20:20:55 +0800

    feat(ui-icons): ✨ 新增多个常规风格图标并导出
    新增了向右箭头、关闭、左箭头、世界地图等多个SVG图标组件，同时在入口文件中统一导出所有图标

---
+++ src/ui/g-icons/regular-chevron-right.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  nonScalingStroke?: boolean
+}>()
+</script>
+
+<template>
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor">
+    <polyline
+      points="5,3 11,8 5,13"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
---
+++ src/ui/g-icons/regular-chrome-close.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps<{
+  nonScalingStroke?: boolean
+}>()
+</script>
+
+<template>
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <line
+      x1="3"
+      y1="3"
+      x2="13"
+      y2="13"
+      stroke="currentColor"
+      stroke-width="1"
+      :vector-effect="nonScalingStroke ? 'non-scaling-stroke' : undefined"
+    />
+    <line
+      x1="13"
+      y1="3"
+      x2="3"
+      y2="13"
+      stroke="currentColor"
+      stroke-width="1"
+      :vector-effect="nonScalingStroke ? 'non-scaling-stroke' : undefined"
+    />
+  </svg>
+</template>
