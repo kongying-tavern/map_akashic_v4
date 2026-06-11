cee6cfe7f415a3a7ce1571c8ec1c8fe8d6310cf2
夕云葛城 <kuroneko7520@outlook.com>
2026-06-11 19:39:32 +0800

    refactor(assets): ♻️ 重构文件缓存逻辑并新增通用可缓存资源请求方法

--- src/api/services/assets/apiDefinitions.ts
+++ src/api/services/assets/apiDefinitions.ts
@@ -4,8 +4,8 @@
 const BASE_URL = import.meta.env.VITE_SERVICE_RESOURCE_URL
 
 const context = {
-  dir: null as Promise<FileSystemDirectoryHandle> | null,
-  tilesetDirs: new Map<string, Promise<FileSystemDirectoryHandle>>(),
+  root: null as Promise<FileSystemDirectoryHandle> | null,
+  folderHandles: new Map<string, Promise<FileSystemDirectoryHandle>>(),
 }
 
 const getUrlMeta = (url: string) => {
@@ -27,6 +27,13 @@ const getUrlMeta = (url: string) => {
   }
 }
 
+const getRoot = () => {
+  if (!context.root) {
+    context.root = navigator.storage.getDirectory()
+  }
+  return context.root
+}
+
 /** 获取 tile 图片资源 */
 export const getTile = async (
   query: {
@@ -39,29 +46,30 @@ export const getTile = async (
   },
   signal?: AbortSignal,
 ) => {
+  signal?.throwIfAborted()
+
   const { pathId, x, y, z, zMapping = 0, extension = 'png' } = query
   const url = `${BASE_URL}/tiles_${pathId}/${z + zMapping}/${x}_${y}.${extension}`
 
   // 优先检查缓存
-  if (!context.dir) {
-    context.dir = navigator.storage.getDirectory()
-  }
-  const dir = await context.dir
   const { foldername, filename } = getUrlMeta(url)
-  if (!context.tilesetDirs.has(foldername)) {
+  if (!context.folderHandles.has(foldername)) {
     // 缓存 tileset 目录句柄，缓存量级不会超过 100 个，因此不做清理。
     // 目前不超过 10 个地图，每地图只有 4 个 level 层级目录。
     // 未来来看不会超过 100 个独立地图，假设句柄占 1KB 内存，即使缓存 400 个句柄也不会对内存和性能形成客观影响。
     // 而缓存句柄的收益远超每次请求都重新获取句柄。
-    context.tilesetDirs.set(foldername, dir.getDirectoryHandle(foldername, { create: true }))
+    const root = await getRoot()
+    context.folderHandles.set(foldername, root.getDirectoryHandle(foldername, { create: true }))
   }
-  const folder = await context.tilesetDirs.get(foldername)!
-  const fileHandle = await folder.getFileHandle(filename, { create: true })
-  const file = await fileHandle.getFile()
+  const folder = await context.folderHandles.get(foldername)!
+  const fileHandle = await folder.getFileHandle(filename).catch(() => null)
 
-  // 文件大于 0 表示写入了有效的图片文件，直接返回
-  if (file.size > 0) {
-    return createImageBitmap(file)
+  if (fileHandle) {
+    const file = await fileHandle.getFile()
+    // 文件大于 0 表示写入了有效的图片文件，直接返回
+    if (file.size > 0) {
+      return createImageBitmap(file)
+    }
   }
 
   // 缓存 miss 的情况
@@ -74,11 +82,53 @@ export const getTile = async (
   if (!res.ok) throw new Error(res.statusText || '请求失败')
 
   const blob = await res.blob()
+  signal?.throwIfAborted()
 
   // 不需要等待缓存写入
-  fileHandle
-    .createWritable()
-    .then((writable) => writable.write(blob).finally(() => writable.close()))
+  folder
+    .getFileHandle(filename, { create: true })
+    .then((handle) => handle.createWritable())
+    .then((stream) => stream.write(blob).then(stream.close).catch(stream.abort))
 
   return createImageBitmap(blob)
+}
+
+/** 缓存优先的资源请求 */
+export const getCacheableAsset = async (url: string, signal?: AbortSignal) => {
+  signal?.throwIfAborted()
+
+  const { foldername, filename } = getUrlMeta(url)
+  if (!context.folderHandles.has(foldername)) {
+    const root = await getRoot()
+    context.folderHandles.set(foldername, root.getDirectoryHandle(foldername, { create: true }))
+  }
+  const folder = await context.folderHandles.get(foldername)!
+  const fileHandle = await folder.getFileHandle(filename).catch(() => null)
+  if (fileHandle) {
+    const file = await fileHandle.getFile()
+    // 文件大于 0 表示写入了有效的图片文件，直接返回
+    if (file.size > 0) {
+      return file
+    }
+  }
+
+  // 缓存 miss 的情况
+  const res = await fetch(url, {
+    mode: 'cors',
+    method: 'GET',
+    signal,
+  })
+  // 请求失败直接不走缓存
+  if (!res.ok) throw new Error(res.statusText || '请求失败')
+
+  const blob = await res.blob()
+  signal?.throwIfAborted()
+
+  // 不需要等待缓存写入
+  folder
+    .getFileHandle(filename, { create: true })
+    .then((handle) => handle.createWritable())
+    .then((stream) => stream.write(blob).then(stream.close).catch(stream.abort))
+
+  return blob
 }
