a72fef3cd4b0bbdf06f0af6e40c3757c3f402718
夕云葛城 <kuroneko7520@outlook.com>
2026-06-12 19:26:47 +0800

    perf(api/utils): ⚡️ 简化 gzip 解压解析逻辑

--- src/api/utils/decompress-gzip.ts
+++ src/api/utils/decompress-gzip.ts
@@ -2,6 +2,5 @@ export const decompress = async <T = unknown>(res: unknown) => {
   if (!(res instanceof Response)) throw new Error('Not a Response')
   const ds = new DecompressionStream('gzip')
   const response = new Response(res.body?.pipeThrough(ds))
-  const text = await response.text()
-  return JSON.parse(text) as T
+  return response.json() as T
 }
