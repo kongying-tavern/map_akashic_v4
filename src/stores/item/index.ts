ad131ca661ebc3dea051761e79564a33d80ebab9
夕云葛城 <kuroneko7520@outlook.com>
2026-06-12 19:27:58 +0800

    feat(stores): ✨ 新增标记相关的pinia状态管理仓库
    新增marker相关的状态存储，封装标记数据的获取、格式化与索引转换，导出用于全局访问的marker store并在stores入口文件中注册

---
+++ src/stores/item/index.ts
@@ -0,0 +1,7 @@
+import { useRequest } from 'alova/client'
+import { defineStore } from 'pinia'
+import Api from '@/api'
+
+export const useMarkerStore = defineStore('marker', () => {
+  return {}
+})
---
+++ src/stores/marker/index.ts
@@ -0,0 +1,44 @@
+import { useRequest } from 'alova/client'
+import { defineStore } from 'pinia'
+import Api from '@/api'
+
+interface MarkerThin {
+  id: string
+  name: string
+  pos: readonly [x: number, y: number]
+}
+
+export const useMarkerStore = defineStore('item', () => {
+  const { data } = useRequest(Api.main.marker_doc.listMarkersByBinary(), {
+    initialData: {
+      markers: [],
+      users: {},
+    },
+  })
+
+  const markerSet = computed(() => {
+    const { markers: source } = data.value
+    const { length } = source
+    const indexArray = new Array(length) as Array<MarkerThin>
+    const indexMap = new Map<string, MarkerThin>()
+    for (let i = 0; i < length; i++) {
+      const marker = source[i]
+      const id = marker.id as unknown as string
+      const [x, y] = marker.position?.split(',') ?? []
+      const pos = [Number(x), Number(y)] as const
+      const thin: MarkerThin = {
+        id,
+        pos,
+        name: marker.markerTitle ?? '',
+      }
+      indexMap.set(id, thin)
+      indexArray[i] = thin
+    }
+    return { indexArray, indexMap }
+  })
+
+  return {
+    indexList: computed(() => toRaw(markerSet.value.indexArray)),
+    indexMap: computed(() => toRaw(markerSet.value.indexMap)),
+  }
+})
