60dc528246c764061789bd2c2393e074dd6f5e03
夕云葛城 <kuroneko7520@outlook.com>
2026-06-17 19:06:47 +0800

    refactor(development page): ♻️ 给开发页地图筛选组件绑定区域编码参数

--- src/pages/development.vue
+++ src/pages/development.vue
@@ -1,16 +1,30 @@
 <script setup lang="ts">
+import { useUrlSearchParams } from '@vueuse/core'
 import MapFilter from '@/components/map-filter/map-filter.vue'
+
+const params = useUrlSearchParams<{
+  selectedAreaCode?: string
+}>('history')
 </script>
 
 <template>
   <div class="page-development w-full h-full flex">
     <!-- 亮色模式开发区域 -->
     <div class="relative flex-1 bg-white">
-      <MapFilter class="absolute left-8 top-8" />
+      <MapFilter
+        v-model:area-code="params.selectedAreaCode"
+        class="absolute left-8 top-8"
+        style="color-scheme: light"
+      />
     </div>
+
     <!-- 暗色模式开发区域 -->
-    <div class="relative flex-1 bg-[#1a1a2e]">
-      <MapFilter class="absolute left-8 top-8" style="color-scheme: dark" />
+    <div class="relative flex-1 bg-#1a1a21">
+      <MapFilter
+        v-model:area-code="params.selectedAreaCode"
+        class="absolute left-8 top-8"
+        style="color-scheme: dark"
+      />
     </div>
   </div>
 </template>
